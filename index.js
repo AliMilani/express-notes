09b7fcde5f4f608d224cb722d9f3414633254852
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-01-25 11:44:46 +0330

    add users route.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const notes = require('./routes/notes');
+const users = require('./routes/users');
 
 const app = express();
 app.use(express.json());
 app.use('/api/notes', notes);
+app.use('/api/users', users);
 
 const dbUri = process.env.DB_URI;
 
